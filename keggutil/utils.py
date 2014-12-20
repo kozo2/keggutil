dabdad3be9a9cf21416ba58c632d1e9a7def31d4
Kozo Nishida <knishida@riken.jp>
2014-12-21 02:11:56 +0900

    initial commit

---
+++ keggutil/utils.py
@@ -0,0 +1,4 @@
+import requests
+
+def keggmapper(ids):
+    return True
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='keggutil',
+    version='0.1.0',
+    description='Utility package for KEGG pathway database',
+    author='Kozo Nishida',
+    author_email='knishida@riken.jp',
+    url='http://github.com/kozo2/keggutil',
+    keywords=['bioinformatics'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=['keggutil'],
+    install_requires=[
+        'requests',
+    ],
+)
